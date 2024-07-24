240e40dffd7c31e9720303427d017ce3123c39bd
Eyal-Lavi <151451590+Eyal-Lavi@users.noreply.github.com>
2024-07-24 11:38:55 +0300

    Update the project to  ' Up Model ' Style  (Program using Startup File )

---
+++ MoviesAPI/Startup.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+using MoviesAPI.APIBehavior;
+using MoviesAPI.Filters;
+
+namespace MoviesAPI
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+            {
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
+            });
+
+            services.AddCors(options =>
+            {
+                var frontendURL = Configuration.GetValue<string>("frontend_url"); // משיכה מ - appsettings.Development.json
+                Console.WriteLine(frontendURL);
+                options.AddDefaultPolicy(builderCors =>
+                {
+                    builderCors.WithOrigins(frontendURL)
+                               .AllowAnyOrigin()// מאפשר לכל מקור לשלוח בקשות
+                               .AllowAnyHeader()// מאפשר כל כותרת בבקשה
+                               .AllowAnyMethod()// מאפשר כל שיטת HTTP (GET, POST, וכו')
+                               .WithExposedHeaders(new string[] { "totalAmountOfRecords" }); // מאפשר חשיפה של הכותרת "totalAmountOfRecords"
+                });
+            });
+
+            services.AddAutoMapper(typeof(Startup));
+
+            services.AddControllers(options =>
+            {
+                options.Filters.Add(typeof(MyExeptionFilter));
+                options.Filters.Add(typeof(ParseBadRequest));
+            }).ConfigureApiBehaviorOptions(BadRequestsBehavior.Parse);
+
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer();
+
+            // הוספת שירותי Swagger
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "MoviesAPI", Version = "v1" });
+            });
+
+            services.AddEndpointsApiExplorer();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILogger<Startup> logger)
+        {
+            // Configure the HTTP request pipeline.
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "MoviesAPI v1"));
+            }
+
+            app.UseHttpsRedirection(); // http to https
+
+            app.UseCors();
+
+            app.UseAuthentication(); // אימות
+
+            app.UseAuthorization(); // הרשאה
+
+            //ReadAndLogResponse(app, logger);
+
+            app.UseRouting(); // שימוש בניווט
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
