6e7f33fbff2715131c558dc2d55fd54299eeebf8
Eyal-Lavi <151451590+Eyal-Lavi@users.noreply.github.com>
2024-07-24 12:56:37 +0300

    Add Pagination to the Client

--- Client/src/genres/IndexGenres.tsx
+++ Client/src/genres/IndexGenres.tsx
@@ -5,16 +5,26 @@ import { genreDTO } from "./genres.model";
 import { urlGenres } from "../endpoints";
 import GenericList from "../utils/GenericList";
 import Button from "../utils/Button";
+import Pagination from "../utils/Pagination";
+import RecordsPerPageSelect from "../utils/RecordsPerPageSelect";
 
 export default function IndexGenres(){
     const [genres , setGenres] = useState<genreDTO[]>();
+    const [totalAmountOfPages , setTotalAmountOfPages] = useState(0); // סכום העמודים הכולל
+    const [recordsPerPage , setRecordsPerPage] = useState(5); // כמות עמודות בדף
+    const [page , setPage] = useState(1); // העמוד הנוכחי
 
     useEffect(()=>{
-        axios.get(urlGenres)
+        axios.get(urlGenres , {
+            params: {page , recordsPerPage}
+        })
             .then((response: AxiosResponse<genreDTO[]>) => {
+                const totalAmountOfRecords = parseInt(response.headers['totalamountofrecords'], 10);
+                setTotalAmountOfPages(Math.ceil(totalAmountOfRecords / recordsPerPage))
                 setGenres(response.data);
             })
-    },[]);
+    },[page , recordsPerPage]);
+
     const handleDelete=(id:number)=>{
         axios.delete(urlGenres , {
             headers:{
@@ -29,11 +39,20 @@ export default function IndexGenres(){
         <>
             <h3>Genres</h3>
             <Link className="btn btn-primary" to='/genres/create'>Create genre</Link>
+
+            <RecordsPerPageSelect onChange={amountOfRecords => {
+                setPage(1);
+                setRecordsPerPage(amountOfRecords);
+            }}/>
+            
+            <Pagination currentPage={page} totalAmountOfPages={totalAmountOfPages} onChange={newPage => setPage(newPage)}/>
             <GenericList list={genres}>
                 <table className="table table-striped">
                     <thead>
-                        <th></th>
-                        <th>Name</th>
+                        <tr>
+                            <th></th>
+                            <th>Name</th>
+                        </tr>
                     </thead>
                     <tbody>
                         {genres?.map(genre => 
@@ -51,4 +70,4 @@ export default function IndexGenres(){
             </GenericList>
         </>
     )
-}0
+}
