d83b0f679318a70297966abec6a88ee752e014f0
Eyal-Lavi <151451590+Eyal-Lavi@users.noreply.github.com>
2024-07-28 18:09:18 +0300

    Add Actor Entity (Actor , ActorCreationDTO , ActorDTO) and add Actors table on the DataBase

---
+++ MoviesAPI/Entities/Actor.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MoviesAPI.Entities
+{
+    public class Actor
+    {
+        public int Id { get; set; }
+        [Required]
+        [StringLength(120)]
+        public string Name { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Biography { get; set; }
+        public string Picture { get; set; }
+    }
+}
