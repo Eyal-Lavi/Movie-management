f40c7059b2ac9b46120cbf340c0d8d95736ca5a2
Eyal-Lavi <151451590+Eyal-Lavi@users.noreply.github.com>
2024-07-28 16:14:07 +0300

    Adjusting the IndexGenre to match IndexEntity

--- Client/src/genres/IndexGenres.tsx
+++ Client/src/genres/IndexGenres.tsx
@@ -1,79 +1,36 @@
-import axios, { AxiosResponse } from "axios";
-import { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
-import { genreDTO } from "./genres.model";
+import { useState } from "react";
 import { urlGenres } from "../endpoints";
-import GenericList from "../utils/GenericList";
-import Button from "../utils/Button";
-import Pagination from "../utils/Pagination";
-import RecordsPerPageSelect from "../utils/RecordsPerPageSelect";
-import customConfirm from "../utils/customConfirm";
+import IndexEntity from "../utils/IndexEntity";
+import { genreDTO } from "./genres.model";
 export default function IndexGenres(){
-    const [genres , setGenres] = useState<genreDTO[]>();
-    const [totalAmountOfPages , setTotalAmountOfPages] = useState(0); // סכום העמודים הכולל
-    const [recordsPerPage , setRecordsPerPage] = useState(5); // כמות עמודות בדף
-    const [page , setPage] = useState(1); // העמוד הנוכחי
-
-    useEffect(()=>{
-       loadData();
-    },[page , recordsPerPage]);
-
-    const loadData = () => {
-        axios.get(urlGenres , {
-            params: {page , recordsPerPage}
-        })
-            .then((response: AxiosResponse<genreDTO[]>) => {
-                const totalAmountOfRecords = parseInt(response.headers['totalamountofrecords'], 10);
-                setTotalAmountOfPages(Math.ceil(totalAmountOfRecords / recordsPerPage))
-                setGenres(response.data);
-            })
-    }
-    const deleteGenre = async (id: number)=>{
-        try{
-            await axios.delete(`${urlGenres}/${id}`);
-            loadData();
-        }
-        catch(error){
-            if(axios.isAxiosError(error) && error.response){
-                console.error(error.response.data);
-            }
-        }
-    }
     return(
         <>
-            <h3>Genres</h3>
-            <Link className="btn btn-primary" to='/genres/create'>Create genre</Link>
-
-            <RecordsPerPageSelect onChange={amountOfRecords => {
-                setPage(1);
-                setRecordsPerPage(amountOfRecords);
-            }}/>
-            
-            <Pagination currentPage={page} totalAmountOfPages={totalAmountOfPages} onChange={newPage => setPage(newPage)}/>
-            <GenericList list={genres}>
-                <table className="table table-striped">
-                    <thead>
-                        <tr>
-                            <th></th>
-                            <th>Name</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {genres?.map(genre => 
-                            <tr key={genre.id}>
-                                <td>
-                                    <Link className="btn btn-success"
-                                     to={`/genres/edit/${genre.id}`}>Edit</Link>
-                                    <Button 
-                                        className="btn btn-danger" 
-                                        onClick={()=> customConfirm(() => deleteGenre(genre.id))}>Delete</Button>
-                                </td>
-                                <td>{genre.name}</td>
+            <IndexEntity<genreDTO>
+                url={urlGenres}
+                createUrl="genres/create"
+                entityName="Genre"
+                title="Genre"
+            >
+                {(genres , buttons) => <>
+                        <thead>
+                            <tr>
+                                <th></th>
+                                <th>Name</th>
                             </tr>
-                        )}
-                    </tbody>
-                </table>
-            </GenericList>
+                        </thead>
+                        <tbody>
+                            {genres?.map(genre => 
+                                <tr key={genre.id}>
+                                    <td>
+                                        {buttons(`genres/edit/${genre.id}`,genre.id)}
+                                    </td>
+                                    <td>{genre.name}</td>
+                                </tr>
+                            )}
+                        </tbody>
+                    </>
+                }
+            </IndexEntity>
         </>
     )
 }
