3b9c5a9617f2a051d1cf25f80bc52ff3821b2d89
Eyal-Lavi <151451590+Eyal-Lavi@users.noreply.github.com>
2024-07-28 15:07:00 +0300

    Built EditEntity that is generic component

---
+++ Client/src/utils/EditEntity.tsx
@@ -0,0 +1,60 @@
+import axios, { AxiosResponse } from "axios";
+import { ReactElement, useEffect, useState } from "react";
+import { useHistory, useParams } from "react-router-dom";
+import DisplayErrors from "./DisplayErrors";
+import Loading from "./Loading";
+
+export default function EditEntity<TCreation , TRead>
+        (props: editEntityProps<TCreation , TRead>){
+
+    const { id }:any =  useParams();
+    const [entity , setEntity] = useState<TCreation>();
+    const [errors , setErrors] = useState<string[]>([]);
+    const history = useHistory();
+    
+    useEffect(()=>{
+        axios.get(`${props.url}/${id}`) // מושך פה את הישות לצורה של TRead
+        .then((response:AxiosResponse<TRead>)=>{
+            const entityData = response.data;
+            setEntity(props.transform(entityData));// ממיר את הישות ל TCreation
+        })
+    },[id]) // רק במידה ו id משתנה
+
+    const edit = async (entityToEdit: TCreation) =>{
+        try{
+            await axios.put(`${props.url}/${id}`, entityToEdit);
+        }
+        catch(error){
+            if(axios.isAxiosError(error) && error.response){
+                setErrors(error.response.data)
+            }
+        }
+        
+        // console.log(`The Entity that have id : ${id} ,
+        //     His name was changed to ${entityToEdit.name} complete`);
+        history.push(props.indexURL);
+    }
+
+    return(
+        <>
+            <h3>Edit {props.entityName}</h3>
+            <DisplayErrors errors={errors} />
+            {entity ? props.children(entity , edit) : <Loading />}  {/* שליחת הפונקציה והישות למי שמכיל את הקומפוננטה הזאת - עריכת ישות */}
+        </>
+    )
+}
+
+interface editEntityProps<TCreation , TRead >{
+    entityName: string;
+    url: string;
+    indexURL: string;
+    transform(entity: TRead): TCreation;
+    children(
+                entity: TCreation,
+                edit: (entity: TCreation)=> void
+            ) : ReactElement;
+}
+
+EditEntity.defaultProps = {
+    transform: (entity: any) => entity ,
+}
