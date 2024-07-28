f5a870ca319f78cc346a2dae63e1ee68e3967be0
Eyal-Lavi <151451590+Eyal-Lavi@users.noreply.github.com>
2024-07-28 15:09:07 +0300

    Adjusting the EditGenre to match EditEntity

--- Client/src/genres/EditGenre.tsx
+++ Client/src/genres/EditGenre.tsx
@@ -1,53 +1,24 @@
-import { useHistory, useParams } from "react-router-dom"
-import GenreForm from "./GenreForm";
-import { useEffect, useState } from "react";
-import axios, { AxiosResponse } from "axios";
 import { urlGenres } from "../endpoints";
-import { genreCreationDTO} from "./genres.model";
-import Loading from "../utils/Loading";
-import DisplayErrors from "../utils/DisplayErrors";
+import EditEntity from "../utils/EditEntity";
+import GenreForm from "./GenreForm";
+import { genreCreationDTO, genreDTO } from "./genres.model";
 
 export default function EditGenre(){
-    const { id }:any =  useParams();
-    const [genre , setGenre] = useState<genreCreationDTO>();
-    const [errors , setErrors] = useState<string[]>([]);
-    const history = useHistory();
-    
-    useEffect(()=>{
-        axios.get(`${urlGenres}/${id}`)
-        .then((response:AxiosResponse<genreCreationDTO>)=>{
-            const data = response.data;
-            setGenre(data)
-        })
-    },[id])
-
-    const edit = async (genreToEdit: genreCreationDTO) =>{
-        try{
-            await axios.put(`${urlGenres}/${id}`, genreToEdit);
-        }
-        catch(error){
-            if(axios.isAxiosError(error) && error.response){
-                setErrors(error.response.data)
-            }
-        }
-        
-        console.log(`The Genre that have id : ${id} ,
-            His name was changed to ${genreToEdit.name} complete`);
-        history.push('/genres');
-    }
-
     return(
         <>
-            <h3>Edit Genre</h3>
-            <DisplayErrors errors={errors} />
-            {genre ? 
-                <GenreForm 
-                enableReinitialize={true}
-                model={genre} 
-                onSubmit={async value => {
-                    const body =  {...value};
-                    edit(body);
-                }}/> : <Loading />}
+            <EditEntity<genreCreationDTO , genreDTO>
+                    url={urlGenres}
+                    entityName="Genres"
+                    indexURL="/genres"
+                >
+                    {(entity , edit) => // 
+                        <GenreForm model={entity}
+                            onSubmit={async value => {
+                                console.log(value);
+                                edit(value);
+                        }}/>
+                    }
+            </EditEntity>
              
         </>
     )
