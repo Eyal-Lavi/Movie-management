a62093cb5824e276e7f7dc5aa0be8368f8c3dbb9
Eyal-Lavi <151451590+Eyal-Lavi@users.noreply.github.com>
2024-08-04 15:29:08 +0300

    Add MovieTheater mappings in AutoMapperProfiles

--- MoviesAPI/Helpers/AutoMapperProfiles.cs
+++ MoviesAPI/Helpers/AutoMapperProfiles.cs
@@ -1,19 +1,28 @@
 ﻿using AutoMapper;
 using MoviesAPI.DTOs;
 using MoviesAPI.Entities;
+using NetTopologySuite.Geometries;
 
 namespace MoviesAPI.Helpers
 {
     public class AutoMapperProfiles: Profile
     {
-        public AutoMapperProfiles()
+        public AutoMapperProfiles(GeometryFactory geometryFactory)
         {
             CreateMap<GenreDTO, Genre>().ReverseMap(); // מיפוי משני הצדדים
             CreateMap<GenreCreationDTO, Genre>();// מיפוי רק  לצד אחד
 
             CreateMap<ActorDTO, Actor>().ReverseMap();
             CreateMap<ActorCreationDTO, Actor>()
-                .ForMember(x => x.Picture, option => option.Ignore());
+                .ForMember(x => x.Picture, option => option.Ignore());//בחלק זה של המיפוי אנו מוודאים כי ערך התמונה לא יוחלף אלא אם כן משתמש קלט תמונה חדשה.
+
+            CreateMap<MovieTheater, MovieTheaterDTO>()
+                .ForMember(x => x.Latitude, dto => dto.MapFrom(prop => prop.Location.Y))
+                .ForMember(x => x.Longitude, dto => dto.MapFrom(prop => prop.Location.X));
+
+            CreateMap<MovieTheaterCreationDTO, MovieTheater>()
+                .ForMember(x => x.Location, x => x.MapFrom(dto =>
+                geometryFactory.CreatePoint(new Coordinate(dto.Longitude,dto.Latitude))));
         }
     }
 }
