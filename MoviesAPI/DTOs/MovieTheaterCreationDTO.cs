6508f136acf4a9c8cf95d27039e7b2c32a78df20
Eyal-Lavi <151451590+Eyal-Lavi@users.noreply.github.com>
2024-08-04 15:23:38 +0300

    Add MovieTheater DTOs (Creation and DTO)

---
+++ MoviesAPI/DTOs/MovieTheaterCreationDTO.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MoviesAPI.DTOs
+{
+    public class MovieTheaterCreationDTO
+    {
+        [Required]
+        [StringLength(maximumLength:75, MinimumLength = 1)]
+        public string Name { get; set; }
+        [Range(-90,90)]
+        public double Latitude { get; set; }
+        [Range(-180, 180)]
+        public double Longitude { get; set; }
+    }
+}
---
+++ MoviesAPI/DTOs/MovieTheaterDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace MoviesAPI.DTOs
+{
+    public class MovieTheaterDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+    }
+}
