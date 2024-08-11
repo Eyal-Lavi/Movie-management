7767bc3890ce218a659d5cae9f7c76e8d36b1451
Eyal-Lavi <151451590+Eyal-Lavi@users.noreply.github.com>
2024-08-11 15:36:37 +0300

    Adding relationships between entities and updating the Data Base

---
+++ MoviesAPI/Entities/MovieTheatersMovies.cs
@@ -0,0 +1,11 @@
+﻿namespace MoviesAPI.Entities
+{
+    public class MovieTheatersMovies
+    {
+        public int MovieTheaterId { get; set; }
+        public int MovieId { get; set; }
+
+        public MovieTheater MovieTheater { get; set; }
+        public Movie Movie{ get; set; }
+    }
+}
---
+++ MoviesAPI/Entities/MoviesGenres.cs
@@ -0,0 +1,11 @@
+﻿namespace MoviesAPI.Entities
+{
+    public class MoviesGenres
+    {
+        public int GenreId { get; set; }
+        public int MovieId { get; set; }
+
+        public Genre Genre { get; set; }
+        public Movie Movie { get; set; }
+    }
+}
